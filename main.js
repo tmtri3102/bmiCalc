bfa60350ae8230c59448ed30c67be6600b0eaab0
tomeytran <tmtri310251@gmail.com>
2024-09-13 09:47:37 +0700

    update input

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
-let weight = parseFloat(prompt("Nhập cân nặng theo kg, ví dụ 65"));
-let height = parseFloat(prompt("Nhập chiều cao theo m, ví dụ 1.75"));
-bmi = weight / (height * height);
-if (bmi < 18.5) {
-	alert("Underweight");
-} else if (bmi < 25.0) {
-	alert("Normal");
-} else if (bmi < 30.0) {
-	alert("Overweight");
-} else {
-	alert("Obese");
+function submit() {
+	let weight = +document.getElementById("weight").value;
+	let height = +document.getElementById("height").value;
+	let result = document.getElementById("result");
+	console.log(weight);
+	console.log(height);
+	bmi = weight / (height * height);
+	if (bmi < 18.5) {
+		result.innerText = "Underweight";
+	} else if (bmi < 25.0) {
+		result.innerText = "Normal";
+	} else if (bmi < 30.0) {
+		result.innerText = "Overweight";
+	} else {
+		result.innerText = "Obese";
+	}
 }
